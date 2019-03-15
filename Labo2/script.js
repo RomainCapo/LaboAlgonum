c443be687842428829a824f3b9ec240f0cf9cdd0
Romain Capocasale <romain.capocasale@he-arc.ch>
2019-03-15 12:20:12 +0100

    modify plot class

--- Labo2/script.js
+++ Labo2/script.js
@@ -1,3 +1,5 @@
+let plot;
+
 //http://www.javascripter.net/faq/plotafunctiongraph.htm
 class Plot{
   constructor(funIndex, zoom){
@@ -7,114 +9,129 @@ class Plot{
   }
 
   //permet de supprimer tout les éléments du canvas
-  _cleanCanvas(ctx)
+  _cleanCanvas()
   {
-    ctx.save();//on sauve le contexte du canvas
+    this.ctx.save();//on sauve le contexte du canvas
 
-    ctx.setTransform(1, 0, 0, 1, 0, 0);//on utilise la matrice identiée pour retrouver le scale par défaut
-    ctx.clearRect(0, 0, canvas.width, canvas.height);//on clean le canvas
+    this.ctx.setTransform(1, 0, 0, 1, 0, 0);//on utilise la matrice identiée pour retrouver le scale par défaut
+    this.ctx.clearRect(0, 0, canvas.width, canvas.height);//on clean le canvas
 
-    ctx.restore();//on restore le canvas
+    this.ctx.restore();//on restore le canvas
   }
 
   //représente les 2 fonctions
   fun1(x) {return Math.sin(x) - x/13;  }
   fun2(x) {return x / (1 - Math.pow(x,2));}
 
+  fun(x, i){
+    switch (i) {
+      case 1:
+        return Math.sin(x) - x/13;
+        break;
+      case 2:
+        return x / (1 - Math.pow(x,2));
+        break;
+      default:
+        return 0;
+        break;
+    }
+  }
+
+  drawRoot(tabRoot){
+    this.ctx.strokeStyle = "rgb(0,0,0)";
+    Object.keys(tabRoot).forEach(key => {
+      this.ctx.beginPath();
+      this.ctx.arc(this.axes.x0 - tabRoot[key] * this.axes.scale,this.axes.y0 -  0, 5, 0, 2 * Math.PI);
+      this.ctx.stroke();
+    });
+  }
 //fonction de dessin sur le canvas
   _draw() {
    let canvas = document.getElementById("canvas");
 
-   let axes={}
-   let ctx=canvas.getContext("2d");
-   //ctx.clearRect(0, 0, canvas.height, canvas.width);
+   this.axes={}
+   this.ctx=canvas.getContext("2d");
 
-   this._cleanCanvas(ctx);
+   this._cleanCanvas();
 
-   axes.x0 = .5*canvas.width;//point tout a gauche du canvas sur l'axe x
-   axes.y0 = .5*canvas.height; //point tout en haut du canvas sur l'axe y
+   this.axes.x0 = .5*canvas.width;//point tout a gauche du canvas sur l'axe x
+   this.axes.y0 = .5*canvas.height; //point tout en haut du canvas sur l'axe y
 
    //on applique différents facteurs en fonction de si l'utilisateurs désire zoomer la fonction
    if(this.zoom)
    {//si le graphe doit etre zoomer
-     axes.scale = 40;
-     axes.graduation = 5
+     this.axes.scale = 40;
+     this.axes.graduation = 5
    }
    else
    {//si le graphe ne doit pas etre zoomer
-     axes.scale = canvas.width / 200;
-     axes.graduation = 20;
+     this.axes.scale = canvas.width / 200;
+     this.axes.graduation = 20;
    }
 
-   this._showAxes(ctx,axes);//on affiche les axes
+   this._showAxes();//on affiche les axes
+   this.ctx.strokeStyle = "rgb(11,153,11)";
+  this._funGraph(this.fun);
 
-   if(this.funIndex == 1)
-   {
-     this._funGraph(ctx,axes,this.fun1,"rgb(11,153,11)");
-   }
-   else if(this.funIndex == 2)
-   {
-     this._funGraph(ctx,axes,this.fun2,"rgb(66,44,255)");
-   }
   }
 
-  _funGraph(ctx,axes,func,color) {
+  _funGraph(func) {
    let xx, yy;//point qui seront utilisé pour dessiner la fonction
-   let x0=axes.x0;
-   let y0=axes.y0;
+   let x0=this.axes.x0;
+   let y0=this.axes.y0;
+   let scale = this.axes.scale;
 
    //borne de dessin de la fonction
-   let iMax = Math.round((ctx.canvas.width-x0));
-   let iMin = Math.round(-x0);
+   this.iMax = Math.round((this.ctx.canvas.width-x0));
+   this.iMin = Math.round(-x0);
 
   //on définit l'épaisseur de la ligne et la couleur de la fonction
-   ctx.lineWidth = 2;
-   ctx.strokeStyle = color;
+   this.ctx.lineWidth = 2;
 
-   //on dessine la courbe entre iMin et iMax par pas de 0.05
-   ctx.beginPath();
-   for (let i=iMin;i<=iMax;i+=0.05)
+   //on dessine la courbe entre this.iMin et this.iMax par pas de 0.05
+   this.ctx.beginPath();
+   for (let i=this.iMin;i<=this.iMax;i+=0.1)
    {
     xx = i;
-    yy = axes.scale*func(xx/axes.scale);//on calcule le point y et on applique un scale en fonction du résultat
+    yy = scale*func(xx/scale, this.funIndex);//on calcule le point y et on applique un scale en fonction du résultat
 
     //si c'est la première itération de la boucle
-    if(i==iMin)
+    if(i==this.iMin)
     {
-      ctx.moveTo(x0+xx,y0-yy);
+      this.ctx.moveTo(x0+xx,y0-yy);
     }
     else
     {
-      ctx.lineTo(x0+xx,y0-yy);
+      this.ctx.lineTo(x0+xx,y0-yy);
     }
    }
-   ctx.stroke();
+   this.ctx.stroke();
 
    //affiche la graduation sur l'axe en fonction du scale
-   for(let i=iMin; i<=iMax;i++)
+   for(let i=this.iMin; i<=this.iMax;i++)
    {
-     if((i/axes.scale) % axes.graduation == 0)
+     if((i/scale) % this.axes.graduation == 0)
      {
-       ctx.fillText((i/axes.scale).toString(), x0 + i - 7, y0 - 5);//les valeur 5 et 7 sont prises arbitrairement pour décaler le texte de la graduation et fonction du graphe
+       this.ctx.fillText((i/scale).toString(), x0 + i - 7, y0 - 5);//les valeur 5 et 7 sont prises arbitrairement pour décaler le texte de la graduation et fonction du graphe
      }
    }
   }
 
   //permet de dessiner les axes sur le canvas
-  _showAxes(ctx,axes) {
-   let w=ctx.canvas.width;
-   let h=ctx.canvas.height;
+  _showAxes() {
+   let w=this.ctx.canvas.width;
+   let h=this.ctx.canvas.height;
    let xmin = 0;
 
-   ctx.beginPath();
-   ctx.strokeStyle = "rgb(128,128,128)";
+   this.ctx.beginPath();
+   this.ctx.strokeStyle = "rgb(128,128,128)";
     // X axis
-   ctx.moveTo(xmin, axes.y0);
-   ctx.lineTo(w,axes.y0);
+   this.ctx.moveTo(xmin, this.axes.y0);
+   this.ctx.lineTo(w,this.axes.y0);
   // Y axis
-   ctx.moveTo(axes.x0, 0);
-   ctx.lineTo(axes.x0,h);
-   ctx.stroke();
+   this.ctx.moveTo(this.axes.x0, 0);
+   this.ctx.lineTo(this.axes.x0,h);
+   this.ctx.stroke();
 
    //dessine la graduation sur les axes
    //7 est pris arbitrairement pour le dessin de la graduation
@@ -123,10 +140,10 @@ class Plot{
    {
      if(i/4 % 20 == 0)
      {
-       ctx.beginPath();
-       ctx.moveTo(i, axes.y0 + 5);
-       ctx.lineTo(i, axes.y0 - 5);
-       ctx.stroke();
+       this.ctx.beginPath();
+       this.ctx.moveTo(i, this.axes.y0 + 5);
+       this.ctx.lineTo(i, this.axes.y0 - 5);
+       this.ctx.stroke();
      }
    }
  }
@@ -135,13 +152,13 @@ class Plot{
 function selectEvent(){
   let plotIndex = getSelectValue();
   let zoom = getRadioValue();
-  let plot = new Plot(plotIndex, zoom);
+  plot = new Plot(plotIndex, zoom);
 }
 
 function changeEvent(){
   let plotIndex = getSelectValue();
   let zoom = getRadioValue();
-  let plot = new Plot(plotIndex, zoom);
+  plot = new Plot(plotIndex, zoom);
 }
 
 function getRadioValue(){
@@ -173,12 +190,9 @@ function getSelectValue(){
   return plotIndex;
 }
 
-function clickEvent(){
-}
-
-
 class Dichotomie{
-  constructor(plot){
+  constructor(index, plot){
+  this.index = index;
 	this.it = 0;
 	this.tab = [];
     this.plot = plot;
@@ -186,19 +200,20 @@ class Dichotomie{
 	{
 		this.display(i, i+1, i);
 	}
-    
+
+  this.ObjectToArray();
   }
 
   display(depart, arrive, i)
   {
     let m = 0;//sera le milieu
 	let Aarrive = arrive;//Sert à tester si à la fin, la norme arrive n'a pas bougé (si c'est le cas, on n'a pas trouvé de racine, à part p-e sur la borne elle-meme et ca sera testé dans le prochain display grâce à la boucle)
-	
-	while((arrive-depart) > 0.1)//difference entre les deux bornes (on peut mettre bien plus petit)
+
+	while((arrive-depart) > 0.01)//difference entre les deux bornes (on peut mettre bien plus petit)
 	{
 		m = (depart+arrive)/2;//on met m au milieu
 		//changer la ligne du dessous de fun1 à fun2 aux 2 endroits afin de changer de graphe
-		if((this.plot.fun1(depart) * this.plot.fun1(m)) <= 0)//si les deux bornes ont des signes différentes dans le graphe, on bouge la borne de droite (c'est sur qu'on aura une racine)
+		if((this.plot.fun(depart, this.index) * this.plot.fun1(m, this.index)) <= 0)//si les deux bornes ont des signes différentes dans le graphe, on bouge la borne de droite (c'est sur qu'on aura une racine)
 		{
 			arrive = m;//On bouge la borne de droite
 		}
@@ -216,16 +231,30 @@ class Dichotomie{
 	}
 	//document.getElementById("racines").innerHTML = "a = " + tab[i]; //sert à afficher sur la page, à remanier
   }
+
+  ObjectToArray(){
+    this.tab = Object.values(this.tab);
+    this.tab = this.tab.sort(function(a, b){return a - b});
+  }
 }
 
-function clickEventAfficher()
+function clickEventDichotomie()
 {
-	//bouton ne sert plus à rien, (on peut supprimer avec le bouton)
+  let plotIndex = getSelectValue();
+  let zoom = getRadioValue();
+
+  let plot = new Plot(plotIndex, zoom);
+	let racines = new Dichotomie(plotIndex, plot);
+  plot.drawRoot(racines.tab);
+
+  let roots = document.getElementById("roots");
+  roots.innerHTML = '';
+  roots.innerHTML += "roots : ";
+  Object.keys(racines.tab).forEach(key => {
+     roots.innerHTML += racines.tab[key] + "; ";
+  });
 }
 
 document.addEventListener("DOMContentLoaded", function(event) {
-	let plot = new Plot();
-	let racines = new Dichotomie(plot);
-  	let tab2 = racines.tab;
-	console.log(tab2);
+	let plot = new Plot(1, true);
 });
