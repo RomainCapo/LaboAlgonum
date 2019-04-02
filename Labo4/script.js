b6b179fc1e309e6332204de1289e3fa391f9dea3
Moulin Vincent <vincent.moulin@he-arc.ch>
2019-04-02 18:35:53 +0200

    Added graph for labo 4

--- Labo4/script.js
+++ Labo4/script.js
@@ -13,3 +13,30 @@ Classe : dlm-A
 
 Équipe numéro : 3
 */
+
+window.onload = function(){
+	myFunction()
+};
+
+function myFunction()
+{
+	console.log("test")
+	let p = functionPlot({
+	  title: 'cos(x) - cos\'(x) - cos\'\'(x)',
+	  target: '#chart',
+	  width: 800,
+	  height: 500,
+	  xAxis: {
+		label: 'x - axis',
+		domain: [-6, 6]
+	  },
+	  yAxis: {
+		label: 'y - axis'
+	  },
+	  data: [
+		{ fn: 'cos(x)', color: 'red' },
+		{ fn: '-x', color : 'blue' },
+		{ fn: 'x^2', color : 'green' }
+	  ]
+	})
+}
