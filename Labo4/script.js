01a12c6983dacdf032dbba7a7d709ef7e5e42b9c
Moulin Vincent <vincent.moulin@he-arc.ch>
2019-04-03 14:45:46 +0200

    Added cos aproximate

--- Labo4/script.js
+++ Labo4/script.js
@@ -15,27 +15,83 @@ Classe : dlm-A
 */
 
 window.onload = function(){
-	myFunction()
+	generateChart()
 };
 
-function myFunction()
+function generateChart()
 {
-	console.log("test")
+	dataPoints = [];
+
+    for(let i = 0; i < 15; i++)
+    {
+        d = {
+            points: [
+			  [1, 1],
+			  [2, 1],
+			  [2, 2],
+			  [1, 2],
+			  [1, 1]
+			],
+            fnType: 'points',
+            graphType: 'polyline'
+            };
+        dataPoints.push(d);
+    }
+
+	//console.log(cosTaylorApproximate(2*Math.PI));
+	
 	let p = functionPlot({
 	  target: '#chart',
-	  width: 800,
-	  height: 500,
+	  width: 700,
+	  height: 400,
+	  grid: true,
 	  xAxis: {
 		label: 'x - axis',
 		domain: [-6, 6]
 	  },
 	  yAxis: {
-		label: 'y - axis'
+		label: 'y - axis',
+		domain: [-4, 4]
 	  },
-	  data: [
-		{ fn: 'cos(x)', color: 'red' },
-		{ fn: '-x', color : 'blue' },
-		{ fn: 'x^2', color : 'green' }
-	  ]
+	  data: dataPoints
 	})
 }
+
+function cosTaylorApproximate(theta, iteration=50)
+{
+	let cos = 0;
+	
+	for(let n = 0; n < iteration; n++)
+	{
+		cos += (Math.pow(-1, n))*(Math.pow(theta, 2*n)/factor(2*n));
+	}
+	
+	return cos;
+}
+
+function factor(n)
+{
+	if (n == 0) {
+		return 1;
+	}
+	else 
+	{
+		return n * factor(n-1);
+	} 
+}
+
+function firstDerivate(f, theta, n, h)
+{
+    let numerator = 8*(f(theta+h/2, n)-f(theta-h/2,n)) - f(theta + h, n) + f(theta - h, n);
+    let denominator = 6*h;
+    return numerator / denominator;
+}
+
+function secondDerivate(f, theta, n, h)
+{
+    let numerator = f(theta + h, n) + f(theta - h, n) - 2*f(theta, n);
+    let denominator = h*h;
+    return numerator / denominator;
+}
+
+
